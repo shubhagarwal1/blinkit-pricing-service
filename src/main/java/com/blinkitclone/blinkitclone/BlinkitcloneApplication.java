a2b589b99db6951de9ddf76825ae62bcbee353f5
MIGHTY1o1 <shubhagarwalcse@gmail.com>
2025-02-04 12:52:56 +0530

    add base for pricing service

---
+++ src/main/java/com/blinkitclone/blinkitclone/BlinkitcloneApplication.java
@@ -0,0 +1,13 @@
+package com.blinkitclone.blinkitclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlinkitcloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlinkitcloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blinkitclone/blinkitclone/BlinkitcloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blinkitclone.blinkitclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlinkitcloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
